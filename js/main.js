0e9d0069ffb5817cac27c3495b53035cc405aeb8
Minkyung Kim <118186510+MINKYUNG05@users.noreply.github.com>
2022-12-23 00:46:03 +0900

    2022.12.23
    at home

--- js/main.js
+++ js/main.js
@@ -43,10 +43,85 @@ $(function () {
     });
 });
 
-//*스크롤에 따 나타나는 요소
+//*스크롤에 따 나타나는 요소 1슬라이더
 $(document).ready(function(){
     $(window).scroll(function(){
         $('.mySwiper').each(function(){
+            var bottom_of_element=$(this).offset().top+$(this).outerHeight()/2;
+            var bottom_of_window=$(window).scrollTop()+$(window).height();
+
+            if(bottom_of_window>bottom_of_element){
+                $(this).animate({'opacity':'1'},700);
+            }
+        });
+    });
+});
+
+
+//*스크롤에 따 나타나는 요소 2슬라이더
+$(document).ready(function(){
+    $(window).scroll(function(){
+        $('#box3_slider').each(function(){
+            var bottom_of_element=$(this).offset().top+$(this).outerHeight()/2;
+            var bottom_of_window=$(window).scrollTop()+$(window).height();
+
+            if(bottom_of_window>bottom_of_element){
+                $(this).animate({'opacity':'1'},700);
+            }
+        });
+    });
+});
+
+
+//*스크롤에 따 나타나는 요소 3슬라이더
+$(document).ready(function(){
+    $(window).scroll(function(){
+        $('#box4_slider').each(function(){
+            var bottom_of_element=$(this).offset().top+$(this).outerHeight()/2;
+            var bottom_of_window=$(window).scrollTop()+$(window).height();
+
+            if(bottom_of_window>bottom_of_element){
+                $(this).animate({'opacity':'1'},700);
+            }
+        });
+    });
+});
+
+
+//*스크롤에 따 나타나는 요소
+$(document).ready(function(){
+    $(window).scroll(function(){
+        $('#box6_slider').each(function(){
+            var bottom_of_element=$(this).offset().top+$(this).outerHeight()/2;
+            var bottom_of_window=$(window).scrollTop()+$(window).height();
+
+            if(bottom_of_window>bottom_of_element){
+                $(this).animate({'opacity':'1'},700);
+            }
+        });
+    });
+});
+
+
+//*스크롤에 따 나타나는 요소
+$(document).ready(function(){
+    $(window).scroll(function(){
+        $('#box5_title').each(function(){
+            var bottom_of_element=$(this).offset().top+$(this).outerHeight()/2;
+            var bottom_of_window=$(window).scrollTop()+$(window).height();
+
+            if(bottom_of_window>bottom_of_element){
+                $(this).animate({'opacity':'1'},700);
+            }
+        });
+    });
+});
+
+
+//*스크롤에 따 나타나는 요소
+$(document).ready(function(){
+    $(window).scroll(function(){
+        $('#box7_title').each(function(){
             var bottom_of_element=$(this).offset().top+$(this).outerHeight()/2;
             var bottom_of_window=$(window).scrollTop()+$(window).height();
 
