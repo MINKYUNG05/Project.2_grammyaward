264e98427480359f738bfeb2c4d5db0ad045ee47
Minkyung Kim <118186510+MINKYUNG05@users.noreply.github.com>
2022-12-22 23:32:23 +0900

    2022.12.22
    at home

---
+++ js/main.js
@@ -0,0 +1,58 @@
+$(function () {
+    $('#global').click(function () {
+        $('#box9 ul li').removeClass("active");
+        $(this).addClass("active");
+        
+        $("#b91").fadeIn("slow");
+        $("#b92").hide();
+        $("#b93").hide();
+        $("#b94").hide();
+
+    });
+
+
+    $('#internet').click(function () {
+        $('#box9 ul li').removeClass("active");
+        $(this).addClass("active");
+        
+        $("#b92").fadeIn("slow");
+        $("#b91").hide();
+        $("#b93").hide();
+        $("#b94").hide();
+
+    });
+
+    $('#users').click(function () {
+        $('#box9 ul li').removeClass("active");
+        $(this).addClass("active");                
+        $("#b93").fadeIn("slow");
+        $("#b91").hide();
+        $("#b92").hide();
+        $("#b94").hide();
+
+    });
+
+    $('#grammys').click(function () {
+        $('#box9 ul li').removeClass("active");
+        $(this).addClass("active");
+        $("#b91").hide();
+        $("#b92").hide();
+        $("#b93").hide();
+        $("#b94").fadeIn("slow");
+
+    });
+});
+
+//*스크롤에 따 나타나는 요소
+$(document).ready(function(){
+    $(window).scroll(function(){
+        $('.mySwiper').each(function(){
+            var bottom_of_element=$(this).offset().top+$(this).outerHeight()/2;
+            var bottom_of_window=$(window).scrollTop()+$(window).height();
+
+            if(bottom_of_window>bottom_of_element){
+                $(this).animate({'opacity':'1'},700);
+            }
+        });
+    });
+});
