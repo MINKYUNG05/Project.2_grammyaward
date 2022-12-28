02e4518407220e4af85636008685c44107e11f2a
Minkyung Kim <118186510+MINKYUNG05@users.noreply.github.com>
2022-12-28 17:59:52 +0900

    2022.12.28
    at work

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ $(function () {
     $('#global').click(function () {
         $('#box9 ul li').removeClass("active");
         $(this).addClass("active");
-        
+
         $("#b91").fadeIn("slow");
         $("#b92").hide();
         $("#b93").hide();
@@ -14,7 +14,7 @@ $(function () {
     $('#internet').click(function () {
         $('#box9 ul li').removeClass("active");
         $(this).addClass("active");
-        
+
         $("#b92").fadeIn("slow");
         $("#b91").hide();
         $("#b93").hide();
@@ -24,7 +24,7 @@ $(function () {
 
     $('#users').click(function () {
         $('#box9 ul li').removeClass("active");
-        $(this).addClass("active");                
+        $(this).addClass("active");
         $("#b93").fadeIn("slow");
         $("#b91").hide();
         $("#b92").hide();
@@ -44,14 +44,14 @@ $(function () {
 });
 
 //*스크롤에 따 나타나는 요소 1슬라이더
-$(document).ready(function(){
-    $(window).scroll(function(){
-        $('.mySwiper').each(function(){
-            var bottom_of_element=$(this).offset().top+$(this).outerHeight()/2;
-            var bottom_of_window=$(window).scrollTop()+$(window).height();
+$(document).ready(function () {
+    $(window).scroll(function () {
+        $('.mySwiper').each(function () {
+            var bottom_of_element = $(this).offset().top + $(this).outerHeight() / 2;
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
 
-            if(bottom_of_window>bottom_of_element){
-                $(this).animate({'opacity':'1'},700);
+            if (bottom_of_window > bottom_of_element) {
+                $(this).animate({ 'opacity': '1' }, 700);
             }
         });
     });
@@ -59,14 +59,14 @@ $(document).ready(function(){
 
 
 //*스크롤에 따 나타나는 요소 2슬라이더
-$(document).ready(function(){
-    $(window).scroll(function(){
-        $('.swiper-wrapper').each(function(){
-            var bottom_of_element=$(this).offset().top+$(this).outerHeight()/2;
-            var bottom_of_window=$(window).scrollTop()+$(window).height();
+$(document).ready(function () {
+    $(window).scroll(function () {
+        $('.swiper-wrapper').each(function () {
+            var bottom_of_element = $(this).offset().top + $(this).outerHeight() / 2;
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
 
-            if(bottom_of_window>bottom_of_element){
-                $(this).animate({'opacity':'1'},700);
+            if (bottom_of_window > bottom_of_element) {
+                $(this).animate({ 'opacity': '1' }, 700);
             }
         });
     });
@@ -74,14 +74,14 @@ $(document).ready(function(){
 
 
 //*스크롤에 따 나타나는 요소 3슬라이더
-$(document).ready(function(){
-    $(window).scroll(function(){
-        $('#box4_slider').each(function(){
-            var bottom_of_element=$(this).offset().top+$(this).outerHeight()/2;
-            var bottom_of_window=$(window).scrollTop()+$(window).height();
+$(document).ready(function () {
+    $(window).scroll(function () {
+        $('#box4_slider').each(function () {
+            var bottom_of_element = $(this).offset().top + $(this).outerHeight() / 2;
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
 
-            if(bottom_of_window>bottom_of_element){
-                $(this).animate({'opacity':'1'},700);
+            if (bottom_of_window > bottom_of_element) {
+                $(this).animate({ 'opacity': '1' }, 700);
             }
         });
     });
@@ -89,14 +89,14 @@ $(document).ready(function(){
 
 
 //*스크롤에 따 나타나는 요소
-$(document).ready(function(){
-    $(window).scroll(function(){
-        $('#box6_slider').each(function(){
-            var bottom_of_element=$(this).offset().top+$(this).outerHeight()/2;
-            var bottom_of_window=$(window).scrollTop()+$(window).height();
+$(document).ready(function () {
+    $(window).scroll(function () {
+        $('#box6_slider').each(function () {
+            var bottom_of_element = $(this).offset().top + $(this).outerHeight() / 2;
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
 
-            if(bottom_of_window>bottom_of_element){
-                $(this).animate({'opacity':'1'},700);
+            if (bottom_of_window > bottom_of_element) {
+                $(this).animate({ 'opacity': '1' }, 700);
             }
         });
     });
@@ -104,14 +104,14 @@ $(document).ready(function(){
 
 
 //*스크롤에 따 나타나는 요소
-$(document).ready(function(){
-    $(window).scroll(function(){
-        $('#box5_title').each(function(){
-            var bottom_of_element=$(this).offset().top+$(this).outerHeight()/2;
-            var bottom_of_window=$(window).scrollTop()+$(window).height();
+$(document).ready(function () {
+    $(window).scroll(function () {
+        $('#box5_title').each(function () {
+            var bottom_of_element = $(this).offset().top + $(this).outerHeight() / 2;
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
 
-            if(bottom_of_window>bottom_of_element){
-                $(this).animate({'opacity':'1'},700);
+            if (bottom_of_window > bottom_of_element) {
+                $(this).animate({ 'opacity': '1' }, 700);
             }
         });
     });
@@ -119,43 +119,28 @@ $(document).ready(function(){
 
 
 //*스크롤에 따 나타나는 요소
-$(document).ready(function(){
-    $(window).scroll(function(){
-        $('#box7_title').each(function(){
-            var bottom_of_element=$(this).offset().top+$(this).outerHeight()/2;
-            var bottom_of_window=$(window).scrollTop()+$(window).height();
+$(document).ready(function () {
+    $(window).scroll(function () {
+        $('#box7_title').each(function () {
+            var bottom_of_element = $(this).offset().top + $(this).outerHeight() / 2;
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
 
-            if(bottom_of_window>bottom_of_element){
-                $(this).animate({'opacity':'1'},700);
+            if (bottom_of_window > bottom_of_element) {
+                $(this).animate({ 'opacity': '1' }, 700);
             }
         });
     });
 });
 
 //*스크롤에 따 나타나는 요소
-$(document).ready(function(){
-    $(window).scroll(function(){
-        $('#box8_tit').each(function(){
-            var bottom_of_element=$(this).offset().top+$(this).outerHeight()/2;
-            var bottom_of_window=$(window).scrollTop()+$(window).height();
+$(document).ready(function () {
+    $(window).scroll(function () {
+        $('#box8_tit').each(function () {
+            var bottom_of_element = $(this).offset().top + $(this).outerHeight() / 2;
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
 
-            if(bottom_of_window>bottom_of_element){
-                $(this).animate({'opacity':'1'},700);
-            }
-        });
-    });
-});
-
-
-//*스크롤에 따 나타나는 요소
-$(document).ready(function(){
-    $(window).scroll(function(){
-        $('#box10tit').each(function(){
-            var bottom_of_element=$(this).offset().top+$(this).outerHeight()/2;
-            var bottom_of_window=$(window).scrollTop()+$(window).height();
-
-            if(bottom_of_window>bottom_of_element){
-                $(this).animate({'opacity':'1'},700);
+            if (bottom_of_window > bottom_of_element) {
+                $(this).animate({ 'opacity': '1' }, 700);
             }
         });
     });
@@ -163,29 +148,64 @@ $(document).ready(function(){
 
 
 //*스크롤에 따 나타나는 요소
-$(document).ready(function(){
-    $(window).scroll(function(){
-        $('#box11tit').each(function(){
-            var bottom_of_element=$(this).offset().top+$(this).outerHeight()/2;
-            var bottom_of_window=$(window).scrollTop()+$(window).height();
+$(document).ready(function () {
+    $(window).scroll(function () {
+        $('#box10tit').each(function () {
+            var bottom_of_element = $(this).offset().top + $(this).outerHeight() / 2;
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
 
-            if(bottom_of_window>bottom_of_element){
-                $(this).animate({'opacity':'1'},700);
+            if (bottom_of_window > bottom_of_element) {
+                $(this).animate({ 'opacity': '1' }, 700);
             }
         });
     });
 });
 
-$(document).ready(function(){
-    $(window).scroll(function(){
-        $('#footer3tit').each(function(){
-            var bottom_of_element=$(this).offset().top+$(this).outerHeight()/2;
-            var bottom_of_window=$(window).scrollTop()+$(window).height();
 
-            if(bottom_of_window>bottom_of_element){
-                $(this).animate({'opacity':'1'},700);
+//*스크롤에 따 나타나는 요소
+$(document).ready(function () {
+    $(window).scroll(function () {
+        $('#box11tit').each(function () {
+            var bottom_of_element = $(this).offset().top + $(this).outerHeight() / 2;
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+            if (bottom_of_window > bottom_of_element) {
+                $(this).animate({ 'opacity': '1' }, 700);
             }
         });
     });
 });
 
+$(document).ready(function () {
+    $(window).scroll(function () {
+        $('#footer3tit').each(function () {
+            var bottom_of_element = $(this).offset().top + $(this).outerHeight() / 2;
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+            if (bottom_of_window > bottom_of_element) {
+                $(this).animate({ 'opacity': '1' }, 700);
+            }
+        });
+    });
+});
+
+
+//팝업열기&닫기
+$(function () {
+    $('#popup-open').click(function () {
+        $('#wrap').css('display', 'block', );
+        $('html, body').css({'overflow': 'hidden', 'height': '100%'}); // 모달팝업 중 html,body의 scroll을 hidden시킴
+        $('#wrap').on('scroll touchmove mousewheel', function(event) { // 터치무브와 마우스휠 스크롤 방지
+    event.preventDefault();
+    event.stopPropagation();
+
+    return false;
+});
+    });
+
+    $('#popup-close').click(function () {
+        $('#wrap').css('display', 'none');
+        $('html, body').css({'overflow': 'auto', 'height': '100%'}); //scroll hidden 해제
+        $('#wrap').off('scroll touchmove mousewheel');
+    });
+});
