1d473d6d1306b447182b6a2227d9257a969f14a8
Minkyung Kim <118186510+MINKYUNG05@users.noreply.github.com>
2023-01-10 14:31:33 +0900

    2023.01.10
    at work

--- js/sub.js
+++ js/sub.js
@@ -1,5 +1,21 @@
 onscroll = function() {
     var nVScroll = document.documentElement.scrollTop || document.body.scrollTop;
-    if(nVScroll > 40) $("#aside").css("position", "absolute"); 
-    else $("#aside").css("position", "relative");
+    if(nVScroll > 1335 && nVScroll < 4275) $("#aside").css("position", "fixed"); 
+    else $("#aside").css("position", "absolute");
   };
+
+
+
+  $(document).ready(function(){
+    $("#news").click(function(){
+      $("#blog-news").fadeIn("slow");
+      $(this).addClass("btline");
+      $("#podcast").removeClass("btline");
+    });
+
+    $("#podcast").click(function(){
+      $("#blog-podcast").fadeIn("slow");
+      $(this).addClass("btline");
+      $("#news").removeClass("btline");
+    });
+  });
