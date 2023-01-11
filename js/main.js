f3fccfff7370682297caa5e0d9514c4219e545b0
Minkyung Kim <118186510+MINKYUNG05@users.noreply.github.com>
2023-01-11 14:07:27 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -205,19 +205,11 @@ $(document).ready(function () {
 $(function () {
     $('.popup-open').click(function () {
         $('#wrap').css('display', 'block', );
-//         $('html, body').css({'overflow': 'hidden', 'height': '100%'}); // 모달팝업 중 html,body의 scroll을 hidden시킴
-//         $('#wrap').on('scroll touchmove mousewheel', function(event) { // 터치무브와 마우스휠 스크롤 방지
-//     event.preventDefault();
-//     event.stopPropagation();
-
-//     return false;
-// });
     });
 
     $('#popup-close').click(function () {
         $('#wrap').css('display', 'none');
-        // $('html, body').css({'overflow': 'auto', 'height': '100%'}); //scroll hidden 해제
-        // $('#wrap').off('scroll touchmove mousewheel');
+    
     });
 });
 
